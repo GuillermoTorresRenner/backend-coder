691edb521b914dc73b90df7e39ccbc6e62688125
LeBateleur <torresrennerguillermo@gmail.com>
2024-06-26 15:56:04 -0400

    Se prueba cambiar el domino de la app para que haga el callback a github sin errores

--- src/utils/passport.config.js
+++ src/utils/passport.config.js
@@ -88,10 +88,7 @@ const initializaPassport = () => {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL:
-          process.env.NODE_ENV === "production"
-            ? `${process.env.PRODUCTION_DOMAIN}/api/sessions/githubCallback`
-            : `${process.env.DEVELOPMENT_DOMAIN}/api/sessions/githubCallback`,
+        callbackURL: `${process.env.PRODUCTION_DOMAIN}/api/sessions/githubCallback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
