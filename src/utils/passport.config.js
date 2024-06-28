acb27754c25c561c2fc5b338c451404650f0d596
LeBateleur <torresrennerguillermo@gmail.com>
2024-06-28 19:30:26 -0400

    se vuelve a cambiar la ur y el callback de github

--- src/utils/passport.config.js
+++ src/utils/passport.config.js
@@ -89,7 +89,8 @@ const initializaPassport = () => {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         callbackURL:
-callbackURL: "https://backend-coder-production-dfae.up.railway.app/api/sessions/githubCallback",      },
+          "https://backend-coder-production-dfae.up.railway.app/api/sessions/githubCallback",
+      },
       async (accessToken, refreshToken, profile, done) => {
         try {
           // Intenta obtener el usuario por correo electrónico
