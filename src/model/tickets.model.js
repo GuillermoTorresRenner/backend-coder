5091aa0b3936d85bcc4ef615f97c3a68c5115713
LeBateleur <torresrennerguillermo@gmail.com>
2024-06-30 23:50:41 -0400

    se arregla proble acon index de los tickets

--- src/model/tickets.model.js
+++ src/model/tickets.model.js
@@ -6,11 +6,8 @@ import mongoose from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 
 const ticketsSchema = mongoose.Schema({
-  code: { type: String, default: uuidv4() },
-  purchase_datetime: { type: Date, default: Date.now() },
+  code: { type: String, default: () => uuidv4() }, // Cambio aquí
+  purchase_datetime: { type: Date, default: Date.now },
   amount: { type: Number, required: true },
   purchaser: { type: String, required: true },
 });
-const TicketsModel = mongoose.model("tickets", ticketsSchema);
-export default TicketsModel;
-24;
